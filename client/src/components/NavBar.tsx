509999a9fa28b22b9eeb31baa153d3d642950a9c
Mayank Gupta <guptamayank9827@gmail.com>
2024-06-26 18:55:48 -0500

    created basic navbar component

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,97 @@
-import React from 'react';
+import { Box, Flex, HStack, IconButton, Stack, useDisclosure, useColorModeValue } from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+type LinkType = {
+  keyword: string,
+  text: string,
+  link: string
+};
+
+interface LinkProps {
+  navlink: LinkType
+};
+
+const LINKS = [
+  {
+    keyword: "projects",
+    text: "Projects",
+    link: "/projects"
+  },
+  {
+    keyword: "skills",
+    text: "Skills",
+    link: "/skills"
+  },
+  {
+    keyword: "photography",
+    text: "Alter Ego",
+    link: "/photography"
+  }
+];
+
+const NavLink = (props:LinkProps) => {
+  const { link, text } = props.navlink;
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      href={link}
+    >
+      {text}
+    </Box>
+  )
+}
+
 
 function NavBar() {
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <div id="navbar-section">
-        NavBar Component
+
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box>MG</Box>  {/* replace with graphic */}
+            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              {LINKS.map((link) => (
+                <NavLink key={link.keyword} navlink={link} />
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Box>Theme</Box>  {/* insert theme switcher here */}
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {LINKS.map((link) => (
+                <NavLink key={link.keyword} navlink={link} />
+              ))}
+            </Stack>
+          </Box>
+          )
+          :
+          null
+        }
+      </Box>
+
     </div>
   );
 }
