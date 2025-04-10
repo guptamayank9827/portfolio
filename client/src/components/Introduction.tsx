e3925e4f85f12b6f6c700409c4e61bc49b71a9e1
Mayank Gupta <guptamayank9827@gmail.com>
2025-04-10 16:56:35 -0500

    added profile image

--- client/src/components/Introduction.tsx
+++ client/src/components/Introduction.tsx
@@ -1,5 +1,6 @@
 // import React from 'react';
 import { Box, Container, Stack, Flex, Heading, Text, useBreakpointValue, Image } from '@chakra-ui/react';
+import ProfilePicture from '../icons/profile-image.png';
 
 function Introduction() {
   return (
@@ -43,8 +44,9 @@ function Introduction() {
             <Image
               alt={'Login Image'}
               objectFit={'cover'}
-              src={'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'}
+              src={ProfilePicture}
               mt={{ base:8, md:0 }}
+              borderRadius="100px"
             />
           </Flex>
         </Stack>
