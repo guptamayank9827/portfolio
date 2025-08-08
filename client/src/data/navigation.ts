108b574e4b1b7ee1a4ee99c0cc591e733bafab7a
Mayank Gupta <guptamayank9827@gmail.com>
2025-08-08 03:19:56 -0500

    added navigation links to data

---
+++ client/src/data/navigation.ts
@@ -0,0 +1,25 @@
+export const TECH_LINKS = [
+  {
+    keyword: "projects",
+    text: "Projects",
+    link: "/tech/#projects"
+  },
+  {
+    keyword: "skills",
+    text: "Skills",
+    link: "/tech/#skills"
+  },
+  {
+    keyword: "alternate",
+    text: "Alter Ego",
+    link: "/photography"
+  }
+];
+
+export const PHOTOGRAPHY_LINKS = [
+  {
+    keyword: "alternate",
+    text: "Alter Ego",
+    link: "/tech"
+  }
+];
