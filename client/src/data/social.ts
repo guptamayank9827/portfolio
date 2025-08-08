63c271f175df0a13675e34e285907048a838eb65
Mayank Gupta <guptamayank9827@gmail.com>
2025-08-08 03:17:37 -0500

    added social links to data

---
+++ client/src/data/social.ts
@@ -0,0 +1,36 @@
+// icons
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+export const SOCIAL_LINKS = [
+  {
+    keyword: "github",
+    title: "GitHub",
+    link: "https://github.com/guptamayank9827",
+    view: "tech",
+    icon: GitHubIcon
+  },
+  {
+    keyword: "linkedin",
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/in/mayank-gupta-099301161/",
+    view: "tech",
+    icon: LinkedInIcon
+  },
+  {
+    keyword: "instagram",
+    title: "Instagram",
+    link: "https://www.instagram.com/mayankgupta9827/",
+    view: "photography",
+    icon: InstagramIcon
+  },
+  {
+    keyword: "email",
+    title: "Email",
+    link: "mailto:mayankgupta9827usa@gmail.com",
+    view: "all",
+    icon: EmailIcon
+  }
+];
